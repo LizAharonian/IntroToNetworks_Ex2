3d4c72825237d26c3e177c0b91e1d9a6552c43d3
OriBenZaken <orinetanel1@gmail.com>
2018-12-03 21:17:09 +0200

    2c - fix

--- client4_a.py
+++ client4_a.py
@@ -1,11 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest_ip = '10.0.2.2'
+from socket import socket, AF_INET, SOCK_DGRAM
+s = socket(AF_INET, SOCK_DGRAM)
+dest_ip = '127.0.0.1'
 dest_port = 12345
-s.connect((dest_ip, dest_port))
 msg = "A" * 15000
-s.send(msg)
-data = s.recv(4096)
+s.sendto(msg, (dest_ip,dest_port))
+data, sender_info = s.recvfrom(2048)
 print "Server sent: ", data
 s.close()
